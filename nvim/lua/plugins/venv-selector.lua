04933f6d4b6b29282c93dbd2250cddb7f89f53f2
Ibrahim Kocyigit <97330725+ibrahim-kocyigit@users.noreply.github.com>
2026-04-20 23:58:44 +0300

    venv-selector.lua: add Python venv switcher with telescope integration

---
+++ nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,30 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  branch = 'regexp',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-telescope/telescope.nvim',
+    'mfussenegger/nvim-dap-python',
+  },
+  ft = 'python',
+  opts = {
+    settings = {
+      options = {
+        notify_user_on_venv_activation = true,
+      },
+      search = {
+        -- Search for venvs in common locations relative to the project root
+        venv = { name = '.venv', type = 'dir' },
+        virtualenv = { name = 'virtualenv', type = 'dir' },
+        pyenv = {
+          command = 'find ' .. vim.fn.expand '~/.pyenv/versions' .. ' -name python3 -type f 2>/dev/null',
+          type = 'python',
+        },
+      },
+    },
+  },
+  keys = {
+    { '<leader>vs', '<cmd>VenvSelect<CR>',       desc = 'Select Python venv' },
+    { '<leader>vc', '<cmd>VenvSelectCached<CR>', desc = 'Select cached Python venv' },
+  },
+}
