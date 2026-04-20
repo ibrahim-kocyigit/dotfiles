b68f64882136a86217bdc148ac44b2b70589002c
Ibrahim Kocyigit <97330725+ibrahim-kocyigit@users.noreply.github.com>
2026-04-20 23:53:29 +0300

    colortheme.lua: switch to darker style, add italic keywords/comments

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,14 +1,25 @@
 return {
-    {
-      "navarasu/onedark.nvim",
-      priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        require('onedark').setup {
-          style = 'dark'
-        }
-        require('onedark').load()
-      end
-    }
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
+        diagnostics = {
+          darker = true,
+        },
+        lualine = {
+          transparent = false,
+        },
+        code_style = {
+          keywords = 'italic',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none',
+          comments = 'italic',
+        },
+      }
+      require('onedark').load()
+    end,
+  },
 }
-
-
