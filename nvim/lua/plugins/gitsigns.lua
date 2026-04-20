d75f127f3263ae4062fa0007f5882c0ec8248172
Ibrahim Kocyigit <97330725+ibrahim-kocyigit@users.noreply.github.com>
2026-04-20 23:44:51 +0300

    gitsigns.lua: add hunk navigation, staging, blame and diff keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -16,5 +16,41 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Jump to next hunk' })
+
+      map('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Jump to previous hunk' })
+
+      -- Actions
+      map('n', '<leader>hs', gs.stage_hunk,                                    { desc = 'Stage hunk' })
+      map('n', '<leader>hr', gs.reset_hunk,                                    { desc = 'Reset hunk' })
+      map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'Stage hunk' })
+      map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'Reset hunk' })
+      map('n', '<leader>hS', gs.stage_buffer,                                  { desc = 'Stage buffer' })
+      map('n', '<leader>hR', gs.reset_buffer,                                  { desc = 'Reset buffer' })
+      map('n', '<leader>hp', gs.preview_hunk,                                  { desc = 'Preview hunk' })
+      map('n', '<leader>hb', function() gs.blame_line { full = true } end,     { desc = 'Blame line' })
+      map('n', '<leader>hd', gs.diffthis,                                      { desc = 'Diff this file' })
+
+      -- Toggles
+      map('n', '<leader>tb', gs.toggle_current_line_blame,                     { desc = 'Toggle line blame' })
+    end,
   },
 }
